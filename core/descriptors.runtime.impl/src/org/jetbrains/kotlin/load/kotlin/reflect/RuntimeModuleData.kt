ea9d1728177a9236458ef17d53825c99c2cc676c
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-02-04 18:13:42 +0300

    Support local classes in reflection

--- core/descriptors.runtime.impl/src/org/jetbrains/kotlin/load/kotlin/reflect/RuntimeModuleData.kt
+++ core/descriptors.runtime.impl/src/org/jetbrains/kotlin/load/kotlin/reflect/RuntimeModuleData.kt
@@ -16,15 +16,18 @@
 
 package org.jetbrains.kotlin.load.kotlin.reflect
 
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
+import org.jetbrains.kotlin.di.InjectorForRuntimeDescriptorLoader
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.JavaToKotlinClassMap
-import org.jetbrains.kotlin.builtins.KotlinBuiltIns
-import org.jetbrains.kotlin.di.InjectorForRuntimeDescriptorLoader
+import org.jetbrains.kotlin.serialization.deserialization.LocalClassResolver
 
 public class RuntimeModuleData private(private val injector: InjectorForRuntimeDescriptorLoader) {
     public val module: ModuleDescriptor get() = injector.getModuleDescriptor()
+
+    public val localClassResolver: LocalClassResolver get() = injector.getDeserializationComponentsForJava().components.localClassResolver
 
     class object {
         public fun create(classLoader: ClassLoader): RuntimeModuleData {
